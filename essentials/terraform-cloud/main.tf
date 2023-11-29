72b202a83005926f073dce89f3099227ecbd41a2
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-11-30 02:59:43 +0330

    Add terraform cloud essentials

---
+++ essentials/terraform-cloud/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.50.0"
+    }
+  }
+
+  required_version = "~> 1.6.3"
+}
+
+provider "tfe" {
+  token = var.tfc_api_token
+}
+
+resource "tfe_organization" "test" {
+  name  = var.tfc_organization_name
+  email = var.tfc_organization_email
+}
---
+++ essentials/terraform-cloud/variables.tf
@@ -0,0 +1,13 @@
+variable "tfc_api_token" {
+  type = string
+}
+
+variable "tfc_organization_name" {
+  type = string
+  default = "PashmakGuru"
+}
+
+variable "tfc_organization_email" {
+  type = string
+  default = "nasiri.amirreza.96@gmail.com"
+}
