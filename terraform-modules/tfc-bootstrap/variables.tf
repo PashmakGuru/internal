9520a5bb648863d3711ff6b7fe6dff6fadd2d412
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-11-30 17:34:15 +0330

    Use terraform module for applying

---
+++ terraform-modules/tfc-bootstrap/variables.tf
@@ -0,0 +1,26 @@
+variable "api_token" {
+  type = string
+  description = "TFC Personal Access Token to use for bootstrapping"
+}
+
+variable "organization_name" {
+  type = string
+  description = "Name of the organization"
+}
+
+variable "organization_email" {
+  type = string
+  description = "Email of the organization"
+}
+
+variable "project_name" {
+  type = string
+  default = "remote-operations"
+  description = "Project name for remote-operation-related tasks"
+}
+
+variable "workspace_name" {
+  type = string
+  default = "remote-operations"
+  description = "Workspace name for remote-operation-related tasks"
+}
