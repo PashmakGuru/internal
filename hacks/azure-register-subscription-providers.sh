68994f895ed33136ddb87c9204a0a3ecc4729f01
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-12-29 20:18:29 +0330

    Add azure-register-subscription-providers.sh

---
+++ hacks/azure-register-subscription-providers.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+provider=(
+    "Microsoft.Compute"
+    "Microsoft.KeyVault"
+    "Microsoft.ContainerService"
+    "Microsoft.Cdn"
+)
+
+check_prequestics() {
+    if ! command -v az > /dev/null; then
+        echo "📛 Azure CLI is not installed."
+        exit 1
+    fi
+}
+
+list_subscriptions() {
+    echo "Your authenticated subscriptions:"
+    az account list | jq -r "map({id, name}) | .[] | [.id, .name] | @tsv" | column -ts $'\t'
+}
+
+create_and_report() {
+    if ! output=$(az account list | jq ".[].id|select(.==\"$subscription\")"); then
+        echo "📛 Azure subscription access is not found in your client. Use the following command to login with access to it."
+        echo "az login"
+        exit 1
+    fi
+
+    az account set --subscription $subscription
+
+    echo "Retgistered Providers:"
+    az provider list --query "[?registrationState=='Registered']" --output table
+
+    echo ""
+    for provider in "${provider[@]}"; do
+        printf "Registering $provider..."
+        az provider register --wait --namespace "$provider"
+        printf " ✅\n"
+    done
+}
+
+main() {
+    check_prequestics
+    list_subscriptions
+    echo ""
+    read -p "Azure Subscription ID: " subscription
+    create_and_report
+}
+
+main
